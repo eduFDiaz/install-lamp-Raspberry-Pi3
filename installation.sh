073e06d9bca97d9b35145dbd699989d46681415d
eduFDiaz <erioxcuba@gmail.com>
2017-01-27 01:03:41 -0600

    phpmyadmin added

---
+++ installation.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+sudo apt-get install -y git
+
+git clone https://github.com/eduFDiaz/install-lamp-Raspberry-Pi3
+
+cd install-lamp-Raspberry-Pi3
+
+sudo chmod -R 777 install-lamp-Raspberry-Pi3
+
+sudo ./install.sh
+
+#Apache
+sudo apt-get install -y apache2
+sudo echo "ServerName localhost" >> /etc/apache2/httpd.conf
+sudo chmod 777 -R /var/www/html/
+sudo systemctl enable apache2
+
+#Php
+sudo apt-get install -y php5 libapache2-mod-php5
+
+#MySQL
+sudo apt-get install -y mysql-server mysql-client php5-mysql
+
+#phpmyadmin
+sudo apt-get install phpmyadmin -y
+sudo ln -fs /usr/share/phpmyadmin /var/www/html/phpmyadmin
+
+#restarting services
+sudo service apache2 restart
+sudo service mysql restart
